75c445f9a10f3a31a8fbe429708cdec62c0114af
Josh Villamarzo <jvillamarzo@bluewaterads.com>
2013-04-05 15:19:15 -0700

    Second commit

---
+++ mysite/polls/tests/__init__.py
@@ -0,0 +1,6 @@
+#from mysite.polls.tests.test_forms import *
+#from mysite.polls.tests.test_models import *
+#from mysite.polls.tests.test_views import *
+from polls.tests.test_forms import *
+from polls.tests.test_models import *
+from polls.tests.test_views import *
